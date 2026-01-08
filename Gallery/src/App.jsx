9588a5a05d87092136c4c1c6915b910bf2aebb54
nidhi <kumarinidhi0594@gmail.com>
2026-01-08 20:40:50 +0100

    Now data on click with condition

--- Gallery/src/App.jsx
+++ Gallery/src/App.jsx
@@ -5,22 +5,27 @@ const App = () => {
   const [userdata, setUserData] = useState([]);
 
   const getData = async () => {
-    const res = await axios.get('https://picsum.photos/v2/list?page=2&limit=100')
+    const res = await axios.get('https://picsum.photos/v2/list?page=2&limit=15')
     setUserData(res.data)
-    console.log(userdata);
-
-
+    // console.log(userdata);
 
   }
 
+  let printUserdata = "No User Data"
 
+  if (userdata.length > 0) {
+    printUserdata = userdata.map(function() {
+      return "Hello"
+    })
+     
+  }
 
   return (
     <div className='text-white bg-black h-screen p-4'>
       <button onClick={getData}
         className='bg-green-600 text-white px-5 py-2 rounded m-4 active:scale-95'> Get Data </button>
       <div>
-        {userdata.map()}
+        {printUserdata}
       </div>
     </div>
 
