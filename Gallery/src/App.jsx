bb348b9773d1e440c7268072d8348f43b8e90d2d
nidhi <kumarinidhi0594@gmail.com>
2026-01-08 20:18:09 +0100

    Getting the data from backend

--- Gallery/src/App.jsx
+++ Gallery/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import axios from "axios"
 
 const App = () => {
+  const getData = async () => {
+   const res = await axios.get('https://picsum.photos/v2/list?page=2&limit=100')
+   console.log(res);
+   
+
+  }
+
+
+
   return (
     <div className='text-white bg-black h-screen p-4'>
-     <button className='bg-green-600 text-white px-5 py-2 rounded m-4 active:scale-95'> Get Data </button>
+      <button onClick={getData}
+        className='bg-green-600 text-white px-5 py-2 rounded m-4 active:scale-95'> Get Data </button>
     </div>
   )
 }
