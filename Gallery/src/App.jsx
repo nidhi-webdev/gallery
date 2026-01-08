5b4aff6fbdf9d2b6071fd15227b0ef086962aa9c
nidhi <kumarinidhi0594@gmail.com>
2026-01-08 19:56:59 +0100

    Button

--- Gallery/src/App.jsx
+++ Gallery/src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-black '>
-      Hello 
+    <div className='text-white bg-black h-screen p-4'>
+     <button className='bg-green-600 text-white px-5 py-2 rounded m-4 active:scale-95'> Get Data </button>
     </div>
   )
 }
