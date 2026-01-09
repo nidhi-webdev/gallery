a488826c099d9de6c41c543310882f9fc2aabd3f
nidhi <kumarinidhi0594@gmail.com>
2026-01-09 19:02:01 +0100

    getting data on the page and also manipulating it

--- Gallery/src/App.jsx
+++ Gallery/src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   const [userdata, setUserData] = useState([]);
 
   const getData = async () => {
-    const res = await axios.get('https://picsum.photos/v2/list?page=2&limit=15')
+    const res = await axios.get('https://picsum.photos/v2/list?page=3&limit=30')
     setUserData(res.data)
     console.log(res.data);
 
@@ -16,7 +16,10 @@ const App = () => {
   if (userdata.length > 0) {
     printUserdata = userdata.map((elem, idx) => {
       return <div key={idx}>
-        <img src={elem.download_url} />
+        <div key={idx} className="h-40 w-50 bg-white">
+          <img src={elem.download_url} alt="Image" className="h-[90%] w-full object-cover" />
+        </div>
+        <h1> {elem.author} </h1>
       </div>
     })
 
@@ -26,7 +29,7 @@ const App = () => {
     <div className='text-white bg-black h-screen p-4 overflow-auto'>
       <button onClick={getData}
         className='bg-green-600 text-white px-5 py-2 rounded m-4 active:scale-95'> Get Data </button>
-      <div>
+      <div className="flex flex-wrap gap-4">
         {printUserdata}
       </div>
     </div>
