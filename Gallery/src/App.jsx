7eeea08f2f16c44d8e0cc2edbef75d6a6e9e8745
nidhi <kumarinidhi0594@gmail.com>
2026-01-09 19:23:22 +0100

    A tag and giving the original source of the image

--- Gallery/src/App.jsx
+++ Gallery/src/App.jsx
@@ -15,9 +15,13 @@ const App = () => {
 
   if (userdata.length > 0) {
     printUserdata = userdata.map((elem, idx) => (
-      <div key={idx} className="bg-white h-40 flex flex-col rounded-2xl">
-        <img src={elem.download_url} alt="Image" className="h-[85%] w-full object-cover" />
-        <h1 className="text-black text-center bold  ">{elem.author}</h1>
+      <div key={idx}>
+        <a href={elem.url}>
+          <div  className="bg-white h-40 flex flex-col rounded-2xl">
+            <img src={elem.download_url} alt="Image" className="h-[85%] w-full object-cover rounded-2xl" />
+            <h1 className="text-black text-center bold text-lg  ">{elem.author}</h1>
+          </div>
+        </a>
       </div>
     ));
 
