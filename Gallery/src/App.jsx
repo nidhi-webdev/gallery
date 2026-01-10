e728135e4e26f18010df458d611df21deff26e57
nidhi <kumarinidhi0594@gmail.com>
2026-01-10 17:15:58 +0100

    Issue Resolved

--- Gallery/src/App.jsx
+++ Gallery/src/App.jsx
@@ -1,17 +1,19 @@
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Contact from './pages/Contact'
 import Home from './pages/Home'
 import About from './pages/About'
 
 const App = () => {
   return (
-    <div>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/About' element={<About />} />
-        <Route path='/Contact' element={<Contact />} />
-      </Routes>
-    </div>
+    <BrowserRouter>
+      
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/About' element={<About />} />
+          <Route path='/Contact' element={<Contact />} />
+        </Routes>
+      
+    </BrowserRouter>
   )
 }
 
--- Gallery/src/main.jsx
+++ Gallery/src/main.jsx
@@ -1,12 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+const rootElement = document.getElementById('root')
+if (!rootElement) {
+  throw new Error('Root element not found!')
+}
 
-
-)
+createRoot(rootElement).render(<App />)
