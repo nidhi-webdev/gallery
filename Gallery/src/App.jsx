b759fec57b449c265e6e21c8f476eaa35ebd8e60
nidhi <kumarinidhi0594@gmail.com>
2026-01-10 14:24:56 +0100

    made one more component and passing the data

--- Gallery/src/App.jsx
+++ Gallery/src/App.jsx
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
+import Cards from "./components/Cards";
 
 const App = () => {
   const [userdata, setUserData] = useState([]);
@@ -17,17 +18,12 @@ const App = () => {
   }, [index])
 
 
-  let printUserdata = <h3 className="text-gray-400 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"> Loading... </h3>
+  let printUserdata = <h3 className="text-gray-300 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold"> Loading... </h3>
 
   if (userdata.length > 0) {
     printUserdata = userdata.map((elem, idx) => (
       <div key={idx}>
-        <a href={elem.url}>
-          <div className="bg-white h-40 flex flex-col rounded-2xl">
-            <img src={elem.download_url} alt="Image" className="h-[85%] w-full object-cover rounded-2xl" />
-            <h1 className="text-black text-center bold text-lg  ">{elem.author}</h1>
-          </div>
-        </a>
+       <Cards elem={elem}  />
       </div>
     ));
 
@@ -35,7 +31,6 @@ const App = () => {
 
   return (
     <div className='text-white bg-black h-screen p-4 overflow-auto'>
-      <div className="text-6xl bg-amber-700 fixed"> {index}</div>
       <div className="grid grid-cols-5 gap-4 w-full">
         {printUserdata}
       </div>
---
+++ Gallery/src/components/Cards.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Cards = (props) => { 
+  return (
+    <div>
+       <a href={props.elem.url}>
+          <div className="bg-white h-40 flex flex-col rounded-2xl">
+            <img src={props.elem.download_url} alt="Image" className="h-[85%] w-full object-cover rounded-2xl" />
+            <h1 className="text-black text-center bold text-lg  ">{props.elem.author}</h1>
+          </div>
+        </a>
+    </div>
+  )
+}
+
+export default Cards
