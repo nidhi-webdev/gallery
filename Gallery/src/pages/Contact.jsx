6b31a7bddd9a4b294cddef8b35a758ee0028ea31
nidhi <kumarinidhi0594@gmail.com>
2026-01-12 15:48:36 +0100

    formate

--- Gallery/src/pages/Contact.jsx
+++ Gallery/src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import { Link, Outlet } from "react-router-dom"
 const Contact = () => {
   return (
     <div className="text-white">
-     <h1> Contact </h1> 
+     
      <Link to="/contact/mobile"> Mobile </Link>
      <Link to="/contact/telephone"> Telephone </Link>
      <Link to="/contact/fax"> Fax </Link>
