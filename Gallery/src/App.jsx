5b0c5c37a0aaffd89caa77cb377b99cbf40425be
nidhi <kumarinidhi0594@gmail.com>
2026-01-13 18:17:26 +0100

    Context api started

--- Gallery/src/App.jsx
+++ Gallery/src/App.jsx
@@ -1,36 +1,10 @@
-import Footer from './components/Footer'
+import React from 'react'
 import Navbar from './components/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Contact from './pages/Contact'
-import About from './pages/About'
-import Notfound from './pages/Notfound'
-import Mobile from './pages/Mobile'
-import Telephone from './pages/Telephone'
-import Fax from './pages/Fax'
-import Course from './pages/course'
-import CourseDetails from './pages/CourseDetails'
 
 const App = () => {
   return (
-    <div className='h-screen bg-black text-white'>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/contact/mobile' element={<Mobile />} />
-        <Route path='/contact/telephone' element={<Telephone />} />
-        <Route path='/contact/fax' element={<Fax />} />
-
-        <Route path='/about' element={<About />} />
-
-        <Route path='/course' element={<Course />} />
-        <Route path='/course/:id' element={<CourseDetails />} />
-
-        <Route path='*' element={<Notfound />} />
-      </Routes>
-      <Footer />
+    <div>
+     <Navbar />
     </div>
   )
 }
--- Gallery/src/components/Navbar.jsx
+++ Gallery/src/components/Navbar.jsx
@@ -1,15 +1,11 @@
-import { Link } from 'react-router-dom'
+import React from 'react'
+import Nav2 from './Nav2'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between px-8 py-4 bg-cyan-700 items-center'>
-     <h2 className='text-xl font-bold'> My Page </h2>
-     <div className='flex gap-8 text-xl font-bold'>
-        <Link to="/"> Home </Link>
-        <Link to="/contact"> Contact </Link>
-        <Link to="/about"> About </Link>
-        <Link to="/course"> Course </Link>
-     </div>
+    <div>
+      Navbar
+      <Nav2 />
     </div>
   )
 }
