4a797fb21b6b457a22e7e691a3f0259a9b7b8316
nidhi <kumarinidhi0594@gmail.com>
2026-01-14 18:12:28 +0100

    New File Button

--- Gallery/src/App.jsx
+++ Gallery/src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar'
+import Button from './components/Button'
 
 const App = () => {
 
   return (
     <div>
      <Navbar />
+     <Button />
     </div>
   )
 }
