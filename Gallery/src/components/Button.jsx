3137486fc9ad42eafc0b7c962167de5fac4f1e47
nidhi <kumarinidhi0594@gmail.com>
2026-01-14 18:15:28 +0100

    Button component

--- Gallery/src/components/Button.jsx
+++ Gallery/src/components/Button.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
 const Button = () => {
+ 
+    const changeTheme = () => {
+        console.log("hello from Button");
+        
+    }
+
+
   return (
     <div>
-      <button></button>
+      <button onClick={changeTheme} className='bg-gray-600 m-3 px-4 py-2 rounded'> Change Theme </button>
     </div>
   )
 }
